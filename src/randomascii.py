e8d4414f2fc1fb44b450520ab78de6b90de976ae
Leonard <mendes_leo@live.com>
2012-06-22 13:55:07 -0300

    Changed the greyscale.

--- src/randomascii.py
+++ src/randomascii.py
@@ -1,4 +1,83 @@
-        ascii_string = ascii_string + possibles[random.randint(0, len(possibles) - 1)]
+'''
+ASCII Art maker
+A (very simple) class for creating an ASCII string
+Picks a random image in a given directory and generates an ASCII art from it.
+Takes in .jpg, .jpeg, .bmp and .png image files.
+Author: leo
+Date: 06/12/2012
+'''
+
+#TODO: Clean up the code. Maybe split it into two different files (the class and the I/O functionality).
+#      Also, try to change the output size according to the environment.
+
+from PIL import Image
+import random
+from bisect import bisect
+import os
+import sys
+
+
+#Size (in pixels) of the output image. I think that making
+#these variables to adapt to some environment will be implementation dependable.
+#Will try to make a UNIX/POSIX one afterwards.
+#leo
+
+_HEIGHT = 160
+_WIDTH = 75
+
+# The following strings represent
+# 16 tonal ranges, from lighter to darker, so that they
+# are the greyscale.
+# An alternative greyscale can be picked at http://local.wasp.uwa.edu.au/~pbourke/dataformats/asciiart/
+
+greyscale = [
+            " ",
+            ".",
+            ",",
+            ":",
+            ";",
+            "+",
+            "=",
+            "o",
+            "a",
+            "e",
+            "0",
+            "$",
+            "@",
+            "A",
+            "#",
+            "M"
+            ]
+
+# There are 16 luminosity bands, of equal sizes. They can be changed accordingly;
+# for instance, to boost contrast.
+
+zonebounds=[16, 32, 48, 64, 80, 96, 128, 144, 160, 176, 192, 208, 224, 240, 255]
+
+class AsciiGenerator(object):
+  #This assumes that correct path checking was done previously.
+  #May want to change the panic mode and not exiting like this.
+  def __init__(self, image_path):
+    try:
+      self.image = Image.open(image_path)
+    except IOError:
+      print 'Could not open image. Are you sure you entered the correct path?\n'
+      print 'Target path: %(target_path)s' % {'target_path':image_path}
+      sys.exit(-1)
+    self.image = self.image.resize((_HEIGHT, _WIDTH),Image.BILINEAR)
+    self.image = self.image.convert("L") # convert to mono
+
+  def __str__(self):
+    ascii_string = ''
+    for height in xrange(0, self.image.size[1]):
+      for width in xrange(0, self.image.size[0]):
+        lum = 255 - self.image.getpixel((width, height))
+        row = bisect(zonebounds, lum)
+        try:
+         possibles = greyscale[row]
+        except IndexError:
+         continue
+      ascii_string = ascii_string + possibles[random.randint(0, len(possibles) - 1)]
       ascii_string = ascii_string + '\n'
     return ascii_string
 
